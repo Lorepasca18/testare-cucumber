8c08b61c45a9b4db79cafdaa4bd2961d77467845
Pasca Lorena Diana <pascalorena18@yahoo.com>
2023-01-06 15:44:54 +0200

    contact us 2

--- cypress/e2e/Contact_Us.feature
+++ cypress/e2e/Contact_Us.feature
@@ -8,7 +8,7 @@ Feature: WebdriverUniversity - Contact Us page
         And I enter an email address
         And I type a comment
         And I click on the submit button
-        Then I should be presented with a successful contact us submission 
+        Then I should be presented with a successful contact us submission
 
 
 
@@ -22,4 +22,15 @@ Feature: WebdriverUniversity - Contact Us page
         Then I should be presented with a unsuccessful contact us submission message
 
 
-    
+    Scenario: Valid Contact Us Form Submission - Using specific data
+        Given I navigate tot the webdrivenuniversity homepage
+        When I click on the contact us button
+        And I type a specific first name "Diana"
+        And I type a specific last name "Pasca"
+        And I type a specific email adress "diana.pasca@gmail.com"
+        And I type a specific word "hello123" and number 6788 within the comment input field
+        And I click on the submit button
+        Then I should be presented with a successful contact us submission
+
+
+
--- cypress/support/step_definitions/ContactUs_Steps.js
+++ cypress/support/step_definitions/ContactUs_Steps.js
@@ -1,5 +1,6 @@
 // <reference types="cypress" />
 import { When,Then} from "@badeball/cypress-cucumber-preprocessor";
+//import { words } from "cypress/types/lodash";
 
 
 When('I type a first name', () => {
@@ -15,9 +16,8 @@ When ('I enter an email address', () => {
     cy.get('[name="email"]').type('pascalorena18@yahoo.com');
 })
 
-When ('I type a comment', () => {
+When ('I type a comment', () => {    
     cy.get('textarea[name="message"]').type("Hello world!");
-    
 })
 
 When ('I click on the submit button', () => {
@@ -26,10 +26,33 @@ When ('I click on the submit button', () => {
 })
 
 Then ('I should be presented with a successful contact us submission', () => {
-    cy.get('h1').should('have.text', 'Thank You for your Message!'); 
-    
+    cy.get('h1').should('have.text', 'Thank You for your Message!');  
 })
+
 
 Then ('I should be presented with a unsuccessful contact us submission message', () => {
     cy.get('body').contains('Error: Invalid email address');
+})  
+
+
+
+When ('I type a specific first name {string}' , (firstName) => {
+    cy.get('[name="first_name"]').type(firstName);
+
+})
+
+When ('I type a specific last name {string}' , (lastName) => {
+    cy.get('[name="last_name"]').type(lastName);
+
+})
+
+When ('I type a specific email adress {string}', (email) => {
+    cy.get('[name="email"]').type(email);
+
+})
+
+When ('I type a specific word {string} and number {int} within the comment input field' , (word, number) => {
+    cy.get('textarea[name="message"]').type(word + number);
+    
+
 })
